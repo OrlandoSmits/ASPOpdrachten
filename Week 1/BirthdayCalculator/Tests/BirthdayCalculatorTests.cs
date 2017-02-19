0717d0031ac89ce85edd37dad804a51f21a201e3
Orlando Smits <orlandosmits@live.nl>
2017-02-19 20:10:32 +0100

    Added Unit Test to check if Form is submitted

---
+++ Week 1/BirthdayCalculator/Tests/BirthdayCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using BirthdayCalculator.Controllers;
+using BirthdayCalculator.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BirthdayCalculator.Tests
+{
+    [TestClass]
+    public class BirthdayCalculatorTests
+    {
+        [TestMethod]
+        public void CanFillInForm()
+        {
+            //Arange
+            BirthdayResponse birthdayRespone = new BirthdayResponse()
+            {
+                Name = "Orlando",
+                Day = 13,
+                Month = 02,
+                Year = 1995
+            };
+
+            HomeController targetController = new HomeController();
+
+            // Act
+            ActionResult result = targetController.BirthdayForm(birthdayRespone);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+
+
+    }
+}
